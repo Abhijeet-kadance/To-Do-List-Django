d032d9ae34b6a086756a543b06fb5b787aca18dd
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-04-03 11:09:07 +0530

    Created ToDo List Base Setup

---
+++ todo/tasks/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.forms import ModelForm
+from .models import *
+
+class TaskForm(forms.ModelForm):
+    Title = forms.CharField(label="Title", max_length=500, widget=forms.TextInput(
+        attrs={'placeholder':'Enter Task Title','class':'form-control form-control-lg'}
+    ))
+    
+    class Meta: 
+        model = Task
+        fields = [
+            'Title',
+        ]
+        
+    def clean(self):
+        super(Task,self).clean()
+        
